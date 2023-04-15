cba245af68b9fabd7c0a4e994f0fbf03db3f9763
Yana Kozhokar <yanakozhokar1996@gmail.com>
2023-04-15 18:58:05 +0300

    add slider's base markup

--- src/components/Slider/Slider.jsx
+++ src/components/Slider/Slider.jsx
@@ -1,5 +1,58 @@
-// import css from './Slider.module.css';
+import Sweater from "../../images/sweater-desk-1x.jpg";
+import Mountain from "../../images/mountain-desk-1x.jpg";
+import Woman from "../../images/woman-desk-1x.jpg";
+import Shoes from "../../images/shoes-desk-1x.jpg";
+import Leg from "../../images/leg-desk-1x.jpg";
+import css from "./Slider.module.css";
 
 export default function Slider() {
-  return <ul></ul>;
+  return (
+    <ul className={css.SliderList}>
+      <li className={css.SliderItem}>
+        <img
+          src={Sweater}
+          alt="Sweater"
+          width={239}
+          height={304}
+          className={css.SliderImg}
+        />
+      </li>
+      <li className={css.SliderItem}>
+        <img
+          src={Mountain}
+          alt="Mountain"
+          width={239}
+          height={304}
+          className={css.SliderImg}
+        />
+      </li>
+      <li className={css.SliderItem}>
+        <img
+          src={Woman}
+          alt="Woman"
+          width={239}
+          height={304}
+          className={css.SliderImg}
+        />
+      </li>
+      <li className={css.SliderItem}>
+        <img
+          src={Shoes}
+          alt="Shoes"
+          width={239}
+          height={304}
+          className={css.SliderImg}
+        />
+      </li>
+      <li className={css.SliderItem}>
+        <img
+          src={Leg}
+          alt="Leg"
+          width={239}
+          height={304}
+          className={css.SliderImg}
+        />
+      </li>
+    </ul>
+  );
 }
