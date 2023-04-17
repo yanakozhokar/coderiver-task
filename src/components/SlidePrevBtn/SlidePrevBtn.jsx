400fc25e8b6163f583e9449c6ccd1669f9ad16e8
Yana Kozhokar <yanakozhokar1996@gmail.com>
2023-04-17 18:18:14 +0300

    slider updated

---
+++ src/components/SlidePrevBtn/SlidePrevBtn.jsx
@@ -0,0 +1,10 @@
+// import { React } from "react";
+// import { useSwiper } from "swiper/react";
+
+// export default function SlidePrevButton() {
+//   const swiper = useSwiper();
+
+//   return (
+//     <button onClick={() => swiper.slidePrev()}>Slide to the next slide</button>
+//   );
+// }
--- src/components/Slider/Slider.jsx
+++ src/components/Slider/Slider.jsx
@@ -1,58 +1,48 @@
+import React, { useEffect } from "react";
 import Sweater from "../../images/sweater-desk-1x.jpg";
 import Mountain from "../../images/mountain-desk-1x.jpg";
 import Woman from "../../images/woman-desk-1x.jpg";
 import Shoes from "../../images/shoes-desk-1x.jpg";
 import Leg from "../../images/leg-desk-1x.jpg";
-import css from "./Slider.module.css";
+import Carousel from "nuka-carousel";
+import "./Slider.css";
 
 export default function Slider() {
+  useEffect(() => {
+    const prevBtn = document.querySelector(
+      ".slider-control-centerleft > button"
+    );
+    const nextBtn = document.querySelector(
+      ".slider-control-centerright > button"
+    );
+    const pagination = document.querySelector(".slider-control-bottomcenter");
+
+    pagination.innerHTML = "";
+    prevBtn.textContent = "";
+    nextBtn.textContent = "";
+  });
+
+  function slidesAmount() {
+    if (window.innerWidth < 768) {
+      return 1;
+    } else if (window.innerWidth >= 768 && window.innerWidth < 1200) {
+      return 2;
+    } else {
+      return 4;
+    }
+  }
+
   return (
-    <ul className={css.SliderList}>
-      <li className={css.SliderItem}>
-        <img
-          src={Sweater}
-          alt="Sweater"
-          width={239}
-          height={304}
-          className={css.SliderImg}
-        />
-      </li>
-      <li className={css.SliderItem}>
-        <img
-          src={Mountain}
-          alt="Mountain"
-          width={239}
-          height={304}
-          className={css.SliderImg}
-        />
-      </li>
-      <li className={css.SliderItem}>
-        <img
-          src={Woman}
-          alt="Woman"
-          width={239}
-          height={304}
-          className={css.SliderImg}
-        />
-      </li>
-      <li className={css.SliderItem}>
-        <img
-          src={Shoes}
-          alt="Shoes"
-          width={239}
-          height={304}
-          className={css.SliderImg}
-        />
-      </li>
-      <li className={css.SliderItem}>
-        <img
-          src={Leg}
-          alt="Leg"
-          width={239}
-          height={304}
-          className={css.SliderImg}
-        />
-      </li>
-    </ul>
+    <Carousel
+      slidesToShow={slidesAmount()}
+      cellSpacing={50}
+      disableDotControls={true}
+    >
+      <img src={Sweater} width={239} height={304} alt="Art" />
+      <img src={Mountain} width={239} height={304} alt="Art" />
+      <img src={Woman} width={239} height={304} alt="Art" />
+      <img src={Shoes} width={239} height={304} alt="Art" />
+      <img src={Leg} width={239} height={304} alt="Art" />
+    </Carousel>
   );
 }
