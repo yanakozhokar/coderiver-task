17562282da43dac2143973e27435fd300528372e
Yana Kozhokar <yanakozhokar1996@gmail.com>
2023-04-17 20:38:02 +0300

    add text of about-section

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -34,10 +34,34 @@ export const App = () => {
           <div className={css.container}>
             <Title>Choose looks</Title>
             <Slider />
+            <Title>More about us</Title>
+            <div className={css.aboutDescription}>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam sed
+                enim sed libero commodo efficitur. Suspendisse et lorem ac neque
+                dictum pellentesque nec sit amet nisl. Fusce rutrum quis purus
+                ut pretium. Vivamus ornare mauris non ligula egestas, accumsan
+                faucibus quam sollicitudin. Duis efficitur lorem tellus. Aliquam
+                non rhoncus felis, porttitor consequat quam. Morbi gravida
+                semper mattis. Nunc ultrices justo in pulvinar convallis.
+                Curabitur dapibus ut tellus eu condimentum. Morbi vitae
+                convallis purus, ac finibus ipsum.
+              </p>
+              <p>
+                Suspendisse nec pharetra turpis. Aenean id nunc id orci aliquam
+                pulvinar eget vitae nisl. In ligula neque, vestibulum vel arcu
+                eu, eleifend vestibulum tortor. Duis facilisis, est et aliquam
+                dapibus, urna mauris sagittis mi, et mattis metus magna vel
+                tellus. Phasellus sit amet volutpat ante, ut condimentum lorem.
+                Quisque auctor sollicitudin dui, vitae egestas nunc suscipit et.
+                Nullam eu quam sem. Donec a nibh molestie, aliquam libero
+                maximus, feugiat velit. Curabitur ac accumsan velit. In hac
+                habitasse platea dictumst.
+              </p>
+            </div>
           </div>
         </section>
       </main>
-      <footer></footer>
     </>
   );
 };
